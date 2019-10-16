17c113400a457cbd34b9c0d927dce37bf0726ffa
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-10-15 19:45:27 -0700

    fixed the DRY stretch goal

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
     <h3>Saved Movies:</h3>
      {props.list.map(movie => (
-      <Link to={`/movies/${movie.id}`} className="saved-movie" key={movie.id}>{movie.title}</Link>
+      <NavLink to={`/movies/${movie.id}`} className="saved-movie" key={movie.id}>{movie.title}</NavLink>
     ))}
     <Link to={`/`} className="home-button">Home</Link>
   </div>
